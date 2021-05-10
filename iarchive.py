070cdc025475d201bd39ea16965ce12a777c4e6b
zlfyuan <741136856@qq.com>
2021-05-10 20:46:17 +0800

    修改

---
+++ iarchive.py
@@ -0,0 +1,123 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import os
+import sys
+import subprocess
+import argparse as apa
+
+from pgy import Pgy
+from send_wechart import SendWeixin
+from send_dingtalk import SendDingTalk
+from send_email import SendEmail
+
+# 1。获得app路径
+# 2。生成ipa包
+# 3。登录蒲公英得到 key （保存key到文件）
+# 4。上传ipa到蒲公英 （添加进度条）
+# 5。通知测试用户
+home_path = os.path.expandvars('$HOME')
+
+
+def get_app_path(project):
+    project_name = project
+    derive_data = home_path + "/Library/Developer/Xcode/DerivedData"
+    print(derive_data)
+    if not os.path.exists(derive_data):
+        print("找不到DerivedData文件 -->没有安装Xcode 或者 重启Xcode")
+        sys.exit(1)
+    with os.scandir(derive_data) as it:
+        project_enable = False
+        for entry in it:
+            if project_name == entry.name.split("-")[0] and entry.is_dir():
+                project_enable = True
+                # print(entry.name)
+                project_path = derive_data + "/" + entry.name + "/Build/Products/Debug-iphoneos"
+                with os.scandir(project_path) as dir:
+                    for file in dir:
+                        if project_name + ".app" == file.name:
+                            appfile_path = project_path + "/" + project_name + ".app"
+                            print(appfile_path)
+                            return appfile_path
+        if not project_enable:
+            print("找不到{0}文件 -->没有安装Xcode 或者 重启Xcode".format(project_name))
+            sys.exit(1)
+
+
+# 编译打包 得到ipa
+def bulidIPA(app_path):
+    dirs = app_path.split("/")
+    dirs.pop()
+    # print(dirs)
+    app_dir = "/".join(dirs)
+    pack_bag_path = app_dir + "/packBagPath"
+    pay_load_path = app_dir + "/PayLoadPath"
+    # print(packBagPath)
+    # print(PayLoadPath)
+    subprocess.call(["rm", "-rf", pack_bag_path])
+    subprocess.call(["mkdir", "-p", pay_load_path])
+    subprocess.call(["cp", "-r", app_path, pay_load_path])
+    subprocess.call(["mkdir", "-p", pack_bag_path])
+    subprocess.call(["cp", "-r", pay_load_path, pack_bag_path])
+    subprocess.call(["rm", "-rf", pay_load_path])
+    os.chdir(pack_bag_path)
+    subprocess.call(["zip", "-r", "./Payload.zip", "."])
+    print("\n打包成功...")
+    subprocess.call(["mv", "payload.zip", "Payload.ipa"])
+    subprocess.call(["rm", "-rf", "./Payload"])
+    return pack_bag_path
+
+def projectName(name):
+    app_path = get_app_path(name)
+    update_des = input("请输入更新的日志描述:")
+    ipa_path = bulidIPA(app_path) + "/Payload.ipa"
+    pgy = Pgy()
+    pgy.uploadIPA(ipa_path, update_des)
+
+
+def sendToTester(sender):
+    # print(sender)
+    try:
+        if sender == "weixin":
+            SendWeixin()
+        elif sender == "dingTalk":
+            SendDingTalk()
+        elif sender == "email":
+            SendEmail()
+        else:
+            raise ValueError("❌缺少参数，查看--help")
+    except ValueError as e:
+        print(e)
+
+
+def showVersion(version):
+    print("version:{0}".format(version))
+
+
+if __name__ == '__main__':
+
+    args = apa.ArgumentParser(prog="i_archive", description='iOS Application archive command tools', epilog='Information end ')
+    #必要参数
+    args.add_argument("-db",'--dabao', type=str, dest="db", default=None, help="你的项目Target")
+
+    args.add_argument("-sd", '--send', type=str, dest="send_target", help=u"发送目标，例如钉钉机器人:dingTalk,企业微信机器人:WeCaht")
+
+    args = args.parse_args()
+    # print("argparse.args=", args, type(args))
+    # print("name = {0}".format(args.name))
+    d = args.__dict__
+    # print(d)
+    if not "db" in d or d["db"] == None:
+        # print("无效项目Target")
+        pass
+    else:
+        projectName(d["db"])
+        # print("db",d["db"])
+        # pass
+
+    if d["send_target"] == None:
+        # print("无效的发送目标")
+        pass
+    else:
+        sendToTester(d["send_target"])
+        # print("send", d["send_target"])
+        # pass
